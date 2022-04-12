bc174bf415db0fbc13e4c3a5e174e211afab6bfd
Utsav Pathak <60766525+titanium59@users.noreply.github.com>
2022-04-12 22:26:46 +0530

    Add files via upload

---
+++ recursion/allIndexes.cpp
@@ -0,0 +1,42 @@
+int allIndexes(int input[], int size, int x, int output[]) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Save all the indexes in the output array passed and return the size of output array.
+     Taking input and printing output is handled automatically.
+  */
+
+    if(size==0)
+        return 0;
+
+
+    if(size==1){
+        if(input[0]==x){
+            output[0]=0;
+            return 1;
+        }
+        else
+            return 0;
+    }
+
+
+
+
+    int ans=allIndexes(input+1,size-1,x,output);
+    //this ans will return the size of the output array
+    //above we are assuming this
+    for(int i=0;i<ans;i++)
+    {
+        output[i]++;
+    }//we are updating the values the output array
+    if(input[0]==x){
+        for(int j=ans;j>0;j--){
+            output[j]=output[j-1];
+        }
+        output[0]=0;
+        ans++;
+        return ans;
+
+        }
+    else
+        return ans;
+    }
